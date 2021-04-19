55353f79fa97e05aad2cfdb0592cfbc4c3697682
Duy Nguyen Thanh <ntduy.cs@gmail.com>
2021-04-19 14:13:34 +0700

    update repository layer

--- src/main/java/io/github/thanhduybk/common/domain/repository/BaseRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.thanhduybk.common.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
-
-@NoRepositoryBean
-public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
-}
--- src/main/java/io/github/thanhduybk/common/domain/repository/DslRepository.java
+++ src/main/java/io/github/thanhduybk/common/domain/repository/DslRepository.java
@@ -1,16 +1,14 @@
 package io.github.thanhduybk.common.domain.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import javax.persistence.EntityManager;
 
-public abstract class BaseRepositoryImpl<T, ID> extends SimpleJpaRepository<T, ID> implements BaseRepository<T, ID> {
+public abstract class DslRepository<T> {
     protected final EntityManager entityManager;
     protected final JPAQueryFactory queryFactory;
 
-    public BaseRepositoryImpl(Class<T> entityClass, EntityManager entityManager) {
-        super(entityClass, entityManager);
+    public DslRepository(EntityManager entityManager) {
         this.entityManager = entityManager;
         this.queryFactory = new JPAQueryFactory(entityManager);
     }
